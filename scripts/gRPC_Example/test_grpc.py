1981ebafef02f496b33d3d8a0ffe0b252a1cd2d4
Madai Arteaga <madai.arteaga@epn.edu.ec>
2021-12-15 22:16:19 -0500

    Successfull configuration of everything. This commit add the summary information

--- scripts/gRPC_Example/test_grpc.py
+++ scripts/gRPC_Example/test_grpc.py
@@ -3,31 +3,28 @@ import grpc
 import coordinates_event_pb2
 import coordinates_event_pb2_grpc
 
-"""
-Simulation from fake phone
-"""
 
-print("Phone sending...")
+print("[gRPC] Coordinates sending...")
 
 channel = grpc.insecure_channel("127.0.0.1:30001")
 stub = coordinates_event_pb2_grpc.ItemServiceStub(channel)
 
 # Update this with desired payload
 user_coordinates = coordinates_event_pb2.EventCoordinatesMessage(
-    userId=300,
-    latitude=-100,
-    longitude=30
+    userId=100,
+    latitude=-87.4,
+    longitude=0.2
 )
 
 user_coordinates_2 = coordinates_event_pb2.EventCoordinatesMessage(
-    userId=400,
-    latitude=-100,
-    longitude=30
+    userId=110,
+    latitude=-87.5,
+    longitude=0.21
 )
 
 response_1 = stub.Create(user_coordinates)
 response_2 = stub.Create(user_coordinates_2)
 
 
-print("Coordinates sent...")
+print("[gRPC] Coordinates sent...")
 print(user_coordinates, user_coordinates_2)
